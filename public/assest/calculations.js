f6e333435ad0b69aff6b801c10aefbb7d115197a
nealchapman1995 <neal.chapman1@gmail.com>
2021-02-02 19:20:51 -0700

    adding to json

--- public/assest/calculations.js
+++ public/assest/calculations.js
@@ -1,9 +1,29 @@
 const abv = document.getElementById('abv');
 const calculate = document.getElementById('calculate');
+const sugar = document.getElementById('sugar');
+const cups = document.getElementById('cups');
 
-function abvCalculate(){
+
+function abvCalculate(){	
 	let abvNumber = (((document.getElementById('original-grav').value) - (document.getElementById('final-grav').value)) * 131.25);
-	abv.innerHTML = (abvNumber.toFixed(2));
+	if (abvNumber === 0) {
+		abv.innerHTML = ("Please enter your gravities")
+	}
+	else{
+		abv.innerHTML = ("Your ABV is " + abvNumber.toFixed(2) + " %");
+	}
 }
 
 document.getElementById('calculate').addEventListener("click", abvCalculate);
+
+function sugarCalculate(){
+	let temp = document.getElementById('temp').value;
+	let co = document.getElementById('co2').value;
+	let qty = document.getElementById('qty').value
+	let sugarNeeded = ((15.195 * qty) * (((co - 3.0378) + (0.050062 * temp)) - (0.00026555 * (temp * temp))));
+	
+	sugar.innerHTML = ("You need " + sugarNeeded.toFixed(1) + " grams of corn sugar");
+	cups.innerHTML = ("or " + (sugarNeeded * .0050223).toFixed(2) + " cups");
+}
+
+document.getElementById('calculate-sugar').addEventListener("click", sugarCalculate);
